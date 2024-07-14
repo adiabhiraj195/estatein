a3b5c70c5c3459b4e76501fb8dd4f03c05e46e91
Aditya Raj <adiabhiraj141@gmail.com>
2024-07-14 22:51:09 +0530

    time to update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,76 @@
 import Container from "@/components/ui/container";
 import HeroSection from "@/components/ui/hero";
 import ShortcutCard from "@/components/ui/hero/shortcut-card";
+import HybridButton from "@/components/ui/hybrid-button";
 import { hero_shortcut_links } from "@/lib/links/hero-shortcut-links";
+import PropertieCard from "@/components/ui/propertie-card";
+
+import propImg from "@/assets/Image (1).png";
 
 export default function Home() {
+
+
+  // const propertie_card = {
+  //   image={propImg}
+  //   prop_name="Seaside Serenity Villa"
+  //   about="A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood... Read More"
+  //   bedroom={3}
+  //   bathroom={2}
+  //   prop_type="villa"
+  //   price={5500000}
+  //   url="/properties/1"
+  // }
+
   return (
     <main className="">
       <Container className="">
         <HeroSection />
 
         <div className="flex justify-between items-center px-20 border-y-2 border-gray">
+
           {hero_shortcut_links.map(({ title, url, image }) => {
             return (
               <ShortcutCard title={title} url={url} image={image} key={title} />
             )
           })}
         </div>
+
+      </Container>
+
+      <Container className="px-20 py-20">
+        <div className="mb-20">
+          <h1 className="text-3xl ">Featured Properties</h1>
+
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-gray-400 font-light ">Explore our handpicked selection of featured properties. Each listing offers a glimpse into exceptional homes and investments available through Estatein.<br /> Click "View Details" for more information.</p>
+
+            <HybridButton href="/properties" className="border border-gray rounded-md text-sm font-light px-3 py-2 hover:bg-purple mr-3">
+              View All Properties
+            </HybridButton>
+          </div>
+        </div>
+
+        {/* property cards  */}
+        {/* /**************************** */}
+        <div className="property-card-container">
+          <PropertieCard
+            image={propImg}
+            prop_name="Seaside Serenity Villa"
+            about="A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood... Read More"
+            bedroom={3}
+            bathroom={2}
+            prop_type="villa"
+            price={5500000}
+            url="/properties/1"
+          />
+        </div>
+
+        <div className="flex justify-between items-center pt-6 border-t border-gray-600 my-10">
+          <p className="font-light text-sm text-gray-400"><span>{1}</span> of {60}</p>
+
+          arrows
+        </div>
+        {/* /*********************************************** */}
       </Container>
     </main>
   );
---
+++ src/components/ui/propertie-card/index.tsx
@@ -0,0 +1,70 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+import { IoBed } from "react-icons/io5";
+import { FaBath, FaBuilding } from "react-icons/fa";
+import HybridButton from '../hybrid-button';
+
+interface PropertieCardtypes {
+    image: StaticImageData;
+    prop_name: string;
+    about: string;
+    bedroom: number;
+    bathroom: number;
+    prop_type: string;
+    price: number;
+    url: string;
+}
+
+export default function PropertieCard({
+    image,
+    prop_name,
+    about,
+    bedroom,
+    bathroom,
+    prop_type,
+    price,
+    url
+}: PropertieCardtypes) {
+    return (
+        <div className='flex flex-col border border-gray p-5 max-w-96 rounded-xl'>
+            <div className='image-container w-full overflow-hidden mb-4'>
+                <Image src={image} alt={prop_name} className='' />
+            </div>
+
+            <h2 className='text-xl mb-2'>{prop_name}</h2>
+
+            <p className='text-sm font-light text-gray-300 mb-4'>{about}</p>
+
+            <div className='features flex items-center mb-3'>
+                <div className=' px-3 py-1 bg-gray  text-gray-200 rounded-2xl mr-2'>
+                    <p className='flex justfy-between items-center text-sm font-light '>
+                        <IoBed className='mr-1' />
+                        {bedroom}-Bedroom
+                    </p>
+                </div>
+                <div className=' px-3 py-1 bg-gray  text-gray-200 rounded-2xl mr-2'>
+                    <p className='flex justfy-between items-center text-sm font-light '>
+                        <FaBath className='mr-1' />
+                        {bathroom}-Bathroom
+                    </p>
+                </div>
+                <div className=' px-3 py-1 bg-gray  text-gray-200 rounded-2xl'>
+                    <p className='flex justfy-between items-center text-sm font-light '>
+                        <FaBuilding className='mr-1' />
+                        {prop_type}
+                    </p>
+                </div>
+            </div>
+
+            {/* <div className=''> */}
+                <p className='font-light text-gray-300 text-sm'>Price</p>
+                <div className='flex justify-between items-center'>
+                    <h2>${price}</h2>
+                    <HybridButton href={url} className='h-1/2 items-end rounded-md bg-purple text-sm font-light px-3 py-2 hover:bg-gray-500'>
+                        View Property Details
+                    </HybridButton>
+                </div>
+            {/* </div> */}
+        </div>
+    )
+}
